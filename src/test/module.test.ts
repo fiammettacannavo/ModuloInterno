d1cb23cebc9a1c838764989e935903c6f23c1a6e
Kero2375 <f.carboni98@gmail.com>
2020-04-09 13:52:52 +0200

    fixata dipendenza ts-loader

--- src/test/module.test.ts
+++ src/test/module.test.ts
@@ -2,11 +2,14 @@ import 'jest';
 import { Model } from '../panels/PredictionPanel/model';
 import { Data } from 'utils/dataTypes';
 
-test("nome_test", ()=>{
+test('nome_test', () => {
     let model = new Model();
     const data = new Data();
-    data.series = [[0,0], [1,1]];
+    data.series = [
+        [0, 0],
+        [1, 1],
+    ];
     model.setData(data);
-    model.setPredictor({algorithm: "RL", coefficients: [0,0]});
+    model.setPredictor({ algorithm: 'RL', coefficients: [0, 0] });
     expect(model.predict()).toBe(0);
 });
