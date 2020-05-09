7f862ce8dfc57232522d8a1a892b60aa1da453a7
Kero2375 <f.carboni98@gmail.com>
2020-05-09 18:36:14 +0200

    Aggiuti algoritmi exp, log modulo interno

---
+++ src/panels/PredictionPanel/strategies/REXP/StrategyREXP.ts
@@ -0,0 +1,66 @@
+import { Strategy } from '../Strategy';
+import { Data, DataIterator } from '../../utils/Data';
+import { Predicted } from '../../utils/Predicted';
+import Predictor from '../../../../common/Predictor';
+import OptionRL from '../../../../common/OptionsRegression';
+
+export class StrategyREXP implements Strategy {
+    predicted: Predicted = new Predicted();
+    data!: Data;
+    toPredict?: number;
+
+    predict(data: Data, predictor: Predictor<OptionRL>) {
+        this.predicted.clear();
+
+        this.toPredict = predictor.getOpt().getToPredict();
+        const base = 1 - (this.toPredict || 0); //the other one
+        const coeff = predictor.getCoefficients();
+
+        const f = (x: number) => {
+            return coeff[0] * Math.exp(coeff[1] * x);
+        };
+
+        if (!data) {
+            throw Error('Data not found');
+        }
+
+        this.data = data;
+
+        let it = new DataIterator(this.data);
+        let val;
+        while ((val = it.next())) {
+            if (val.a || val.b) {
+                this.predicted.addValues({ value: f(base === 0 ? val.a : val.b), time: val.time });
+            }
+        }
+
+        return this.predicted;
+    }
+
+    saveToInflux() {
+        const index = this.predicted.size() - 1;
+
+        $.post({
+            url: 'http://localhost:8086/write?db=telegraf',
+            data:
+                'predictionREXP value=' +
+                this.predicted.getAt(index).value +
+                ' ' +
+                this.predicted.getAt(index).time +
+                '000000', // + zeros for wrong time format
+        });
+
+        const time = this.predicted.getAt(index).time;
+        let diff;
+        if (!this.toPredict || this.toPredict === 0) {
+            diff = this.data.getAt(index).a - this.predicted.getAt(index).value;
+        } else {
+            diff = this.data.getAt(index).b - this.predicted.getAt(index).value;
+        }
+
+        $.post({
+            url: 'http://localhost:8086/write?db=telegraf',
+            data: 'predictionDiffREXP value=' + Math.abs(diff) + ' ' + time + '000000', // + zeros for wrong time format
+        });
+    }
+}
--- src/panels/PredictionPanel/strategies/strategies.ts
+++ src/panels/PredictionPanel/strategies/strategies.ts
@@ -1,8 +1,12 @@
 import { PureComponent } from 'react';
 import { StrategyRL } from './RL/StrategyRL';
+import { StrategyREXP } from './REXP/StrategyREXP';
+import { StrategyRLOG } from './RLOG/StrategyRLOG';
 import { StrategySVM } from './SVM/StrategySVM';
 import { Strategy } from './Strategy';
 import { ConfigRL } from './RL/ConfigRL';
+import { ConfigREXP } from './REXP/ConfigREXP';
+import { ConfigRLOG } from './RLOG/ConfigRLOG';
 import { ConfigSVM } from './SVM/ConfigSVM';
 import Option from '../../../common/Options';
 import OptionRL from '../../../common/OptionsRegression';
@@ -11,14 +15,20 @@ import OptionSVM from '../../../common/OptionsSVM';
 export const strategies: { [index: string]: Strategy } = {
     RL: new StrategyRL(),
     SVM: new StrategySVM(),
+    REXP: new StrategyREXP(),
+    RLOG: new StrategyRLOG(),
 };
 
 export const configs: { [index: string]: typeof PureComponent } = {
     RL: ConfigRL,
     SVM: ConfigSVM,
+    REXP: ConfigREXP,
+    RLOG: ConfigRLOG,
 };
 
 export const options: { [index: string]: Option } = {
     RL: new OptionRL(),
     SVM: new OptionSVM(),
+    REXP: new OptionRL(),
+    RLOG: new OptionRL(),
 };
